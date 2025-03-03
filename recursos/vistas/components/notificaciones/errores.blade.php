c931263d1a3e726651d59f42dcfc578062c2831c
fadrian06 <franyeradriansanchez@gmail.com>
2025-03-03 12:28:56 -0400

    support for success messages

--- recursos/vistas/components/notificaciones/errores.blade.php
+++ recursos/vistas/components/notificaciones/errores.blade.php
@@ -2,13 +2,25 @@
 <script>
   document.addEventListener('DOMContentLoaded', () => {
     let errores = `{{!! json_encode(flash()->display('errores')) !!}}`
+    let mensajes = `{{!! json_encode(flash()->display('mensajes')) !!}}`
+
     errores = errores.substring(1, errores.length - 1)
-    errores = JSON.parse(errores)
+    errores = JSON.parse(errores) || {}
+
+    mensajes = mensajes.substring(1, mensajes.length - 1)
+    mensajes = JSON.parse(mensajes) || {}
 
     for (const error of Object.values(errores)) {
       new Noty({
         type: 'error',
         text: `<i class="bx bx-message-error"></i> ${error}`
+      }).show()
+    }
+
+    for (const mensaje of mensajes) {
+      new Noty({
+        type: 'success',
+        text: `<i class="bx bx-message-success"></i> ${mensaje}`
       }).show()
     }
   })
